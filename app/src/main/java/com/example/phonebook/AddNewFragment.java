765564e00314ac46f7c0a2dd57a0898693452f86
AmaBirago <julia_asante@outlook.com>
2020-06-15 16:58:59 +0000

    likkle update

--- app/src/main/java/com/example/phonebook/AddNewFragment.java
+++ app/src/main/java/com/example/phonebook/AddNewFragment.java
@@ -1,9 +1,9 @@
 package com.example.phonebook;
 
+import android.os.Build;
 import android.os.Bundle;
-
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +14,19 @@ import android.view.ViewGroup;
  */
 public class AddNewFragment extends Fragment {
 
+
+
     public AddNewFragment() {
         // Required empty public constructor
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_add_new, container, false);
     }
 }
--- app/src/main/java/com/example/phonebook/ContactsFragment.java
+++ app/src/main/java/com/example/phonebook/ContactsFragment.java
@@ -2,6 +2,7 @@ package com.example.phonebook;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -23,6 +24,12 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        /*hiding app bar for this fragment, not necessary anymore but now we know how to do it lol
+        View layout = inflater.inflate(R.layout.fragment_contacts, container, false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();*/
+
+
         return inflater.inflate(R.layout.fragment_contacts, container, false);
     }
 }
