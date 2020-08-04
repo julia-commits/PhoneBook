63aca4403863ab5670ddd3b0665de96eb3e32f0d
AmaBirago <julia_asante@outlook.com>
2020-08-04 21:09:42 +0000

    **FAB-image upload implemented

--- app/src/main/java/com/example/phonebook/Activities/MainActivity.java
+++ app/src/main/java/com/example/phonebook/Activities/MainActivity.java
@@ -1,23 +1,18 @@
 package com.example.phonebook.Activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.Window;
 import android.widget.FrameLayout;
-import android.widget.Toast;
+
 
 import com.example.phonebook.Fragments.AddNewFragment;
 import com.example.phonebook.Fragments.ContactsFragment;
 import com.example.phonebook.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
 
--- app/src/main/java/com/example/phonebook/Fragments/AddNewFragment.java
+++ app/src/main/java/com/example/phonebook/Fragments/AddNewFragment.java
@@ -1,6 +1,11 @@
 package com.example.phonebook.Fragments;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -10,20 +15,29 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.phonebook.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Objects;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -37,8 +51,13 @@ public class AddNewFragment extends Fragment {
         // Required empty public constructor
     }
 
+    //variables
     private Toolbar toolbar;
-
+    ImageView profile;
+    FloatingActionButton fab;
+    private static final int REQUEST_CAMERA = 1;
+    private static final int SELECT_FILE = 2;
+    Uri imageUri;
 
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -46,45 +65,98 @@ public class AddNewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        //toolbar menu options
         setHasOptionsMenu(true);
         Objects.requireNonNull(((AppCompatActivity) Objects.requireNonNull(getActivity())).getSupportActionBar()).show();
 
+
         View view = inflater.inflate(R.layout.fragment_add_new, container, false);
 
-
-        // Inflate the layout for this fragment
-
-        //datepicker
-        dateSelect = (EditText)view.findViewById(R.id.date);
-        dateSelect.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
+        //fab icon image capture
+        profile = (ImageView) view.findViewById(R.id.profile);
+        fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Calendar currentDate = Calendar.getInstance();
+            public void onClick(View view) {
+                final AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext()); //Read Update
+                alertDialog.setTitle("Choose:");
+                alertDialog.setItems(new CharSequence[]
+                        {"Camera", "Gallery", "Cancel"},
+                        new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch(which) {
+                            case 0:
+                                Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                startActivityForResult(camera, REQUEST_CAMERA);
+                                break;
 
-                int mDay = currentDate.get(Calendar.DAY_OF_MONTH);
-                int mMonth = currentDate.get(Calendar.MONTH);
-                int mYear = currentDate.get(Calendar.YEAR);
-                DatePickerDialog datePickerDialog = new DatePickerDialog(Objects.requireNonNull(getActivity()), new DatePickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                                month = month + 1;
-                                String date = dayOfMonth + "/" + month + "/" + year;
-                                dateSelect.setText(date);
-                            }
-                        },
-                        mDay,
-                        mMonth,
-                        mYear);
-                datePickerDialog.show();
+                            case 1:
+                                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                                startActivityForResult(gallery, SELECT_FILE);
+                                break;
 
+                            case 2:
+                                dialog.cancel();
+                        }
+                    }
+                });
+
+                alertDialog.create().show();
             }
+
         });
 
-        //for our actionbar
-        setHasOptionsMenu(true);
+
+
+    //datepicker
+    dateSelect =(EditText)view.findViewById(R.id.date);
+        dateSelect.setOnClickListener(new View.OnClickListener()
+
+    {
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        public void onClick (View v){
+        Calendar currentDate = Calendar.getInstance();
+
+        int mDay = currentDate.get(Calendar.DAY_OF_MONTH);
+        int mMonth = currentDate.get(Calendar.MONTH);
+        int mYear = currentDate.get(Calendar.YEAR);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(Objects.requireNonNull(getActivity()), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                dateSelect.setText(date);
+            }
+        },
+                mDay,
+                mMonth,
+                mYear);
+        datePickerDialog.show();
+
+    }
+    });
+
         return view;
 
+}
+
+
+//saving captured image
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            assert extras != null;
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            profile.setImageBitmap(imageBitmap);
+        }
+        else if(requestCode == SELECT_FILE){
+            if(data!=null) {
+                imageUri = data.getData();
+                profile.setImageURI(imageUri);
+            }
+
+        }
     }
 
 
