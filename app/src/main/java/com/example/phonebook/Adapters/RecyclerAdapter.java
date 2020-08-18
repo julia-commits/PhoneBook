a4c87172672483a3d54d3049ea1a946a5d26bdfb
silaslawer <slawer31@gmail.com>
2020-08-18 12:45:55 +0000

    rework contact recycler adapter

--- app/src/main/java/com/example/phonebook/Adapters/RecyclerAdapter.java
+++ app/src/main/java/com/example/phonebook/Adapters/RecyclerAdapter.java
@@ -2,9 +2,14 @@ package com.example.phonebook.Adapters;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,7 +21,7 @@ import com.example.phonebook.Viewholders.contactsViewHolder;
 import java.util.ArrayList;
 
 
-public class RecyclerAdapter extends RecyclerView.Adapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ContactItemHolder> {
     private Context context;
     private ArrayList <Contact> listContacts;
     private ArrayList<Contact> mArrayList;
@@ -33,23 +38,38 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ContactItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //inflating item layout
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.profile_card, parent, false);
-        contactsViewHolder cv = new contactsViewHolder(v);
+        RecyclerAdapter.ContactItemHolder cv = new RecyclerAdapter.ContactItemHolder(v);
         return cv;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(ContactItemHolder holder, int position) {
         Contact contact = listContacts.get(position);
         holder.contactName.setText(contact.getF_name());
         holder.contactNumber.setText(contact.getPhone());
-        holder.contactImage.setImage(contact.getProfile_pic());
+
+        Bitmap bitmap = BitmapFactory.decodeByteArray(contact.getProfile_pic(), 0, contact.getProfile_pic().length);
+        holder.contactImage.setImageBitmap(bitmap);
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listContacts.size();
+    }
+
+    static class ContactItemHolder extends RecyclerView.ViewHolder {
+        TextView contactName,contactNumber;
+        ImageView contactImage;
+
+        public ContactItemHolder(@NonNull View itemView) {
+            super(itemView);
+            contactName = itemView.findViewById(R.id.contactName);
+            contactNumber = itemView.findViewById(R.id.contactNumber);
+            contactImage = itemView.findViewById(R.id.contactImage);
+        }
     }
 }
